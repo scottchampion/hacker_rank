c713bd6f6dd284eb36401ddf5e5f122a2816cd5d
Scott Champion <champion@uw.edu>
2016-04-16 22:12:30 -0700

    Completed capitalize exercise

---
+++ capitalize.py
@@ -0,0 +1,19 @@
+S = raw_input()
+L = ""
+
+# Seems like a poor implementation, but this method does not strip space chars
+for char in range(len(S)):
+	if char == 0:
+		L = L + S[char].upper()
+	elif S[char - 1] == ' ':
+		L = L + S[char].upper()
+	else:
+		L = L + S[char]
+
+print L
+
+
+# This implementation strips spaces
+# import string
+# S = raw_input()
+# print string.capwords(S)
