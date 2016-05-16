9c2fc0d84430bead36971b3a5cda34d3737f9333
Scott Champion <champion@uw.edu>
2016-05-15 19:54:46 -0700

    Completed py_the_captains_room exercise

---
+++ py_the_captains_room.py
@@ -0,0 +1,16 @@
+# Gather user input.
+K = int(raw_input())
+room_assignments = map(int, raw_input().split())
+
+# Make set of occupied rooms
+occupied_rooms_set = set(room_assignments)
+
+for i in occupied_rooms_set:
+	try:
+		# able to remove i from room_assignments twice, not captain's room
+		room_assignments.remove(i)
+		room_assignments.remove(i)
+		continue
+	except:
+		# unable to remove i from room_assignments twice. found captain's room
+		print i
