e47daf88873a50fd8d40a20d4c0a2e3ead0903c8
Scott Champion <champion@uw.edu>
2016-05-23 21:28:32 -0700

    Completed py_check_strict_superset

--- py_check_strict_superset.py
+++ py_check_strict_superset.py
@@ -1,10 +1,12 @@
 A = set(raw_input().split())
+flag = True
 
-B = set()
 for i in range(int(raw_input())):
-	# Union set B with set input
-	B |= set(raw_input().split())
+	if flag == True:
+		# Union set B with set input
+		B = set(raw_input().split())
 
-# Print "True" if A contains B and A - B is not empty
-print A >= B and bool(len(A.difference(B)))
-	
+		# Print "True" if A contains B and A - B is not empty
+		flag = flag and A >= B and bool(len(A.difference(B)))
+
+print flag
