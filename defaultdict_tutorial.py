4aa4e92be5f65082bbdfdbc9734c8e9ab7eaff6a
Scott Champion <champion@uw.edu>
2016-05-29 14:18:22 -0700

    Made improvements to defaultdict_tutorial

--- defaultdict_tutorial.py
+++ defaultdict_tutorial.py
@@ -5,20 +5,13 @@ A, B = map(int, raw_input().split())
 GroupA, GroupB = defaultdict(list), list()
 
 # Collect A words for group A 
-i = int(1)
 for a in range(A):
-	GroupA[raw_input()].append(i)
-	i += 1
+	GroupA[raw_input()].append(a + 1)
 
 # Collect B words for group B
 for b in range(B):
 	GroupB.append(raw_input())
 
-# Print the index positions of GroupA for all words in GroupB
+# Print the index positions of GroupA for all words in GroupB or a -1
 for b in GroupB:
-	# print -1 if b is not listed in GroupA
-	if len(GroupA[b]) < 1:
-		print '-1'
-	# print space-separated list of indexes in GroupA 
-	else:
-		print ' '.join(map(str, GroupA[b]))
+	print (' '.join(map(str, GroupA[b])) or -1)
