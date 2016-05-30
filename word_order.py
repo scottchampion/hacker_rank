d51f2a88f3e84f2154b055438534432bfd485116
Scott Champion <champion@uw.edu>
2016-05-29 17:11:49 -0700

    Add comments to word_order exercise

--- word_order.py
+++ word_order.py
@@ -1,13 +1,16 @@
 from collections import OrderedDict
 
-
+# Use OrderedDict to iterate through words in order entered
 words = OrderedDict()
 
+# Prompt user for number of words to expect
 for i in range(int(raw_input())):
 	word = raw_input()
+	# Add word to dictionary; set count = 1 if cannot increment existing count
 	try: words[word] += 1
 	except: words[word] = 1
 
+# Print count of unique words in dictionary
 print len(words)
 
 for word in words:
